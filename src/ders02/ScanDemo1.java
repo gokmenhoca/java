7667821666db3936d64eef14a9e4d7eb11f9c0b2
Mustafa GÖKMEN <gokmen@selcuk.edu.tr>
2024-10-05 13:08:16 +0300

    Correction in print line command

--- src/ders02/ScanDemo1.java
+++ src/ders02/ScanDemo1.java
@@ -6,7 +6,7 @@ public class ScanDemo1 {
     public static void main(String[] args) {
         String  userName = "Ruhi BUNALIM";
         Scanner kb       = new Scanner(userName);
-        System.out.printf("Merhaba, " + kb.nextLine());
+        System.out.print("Merhaba, " + kb.nextLine());
 
         while (kb.hasNext()) {
             System.out.print(" " + kb.next());
